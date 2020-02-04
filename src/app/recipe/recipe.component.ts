3a3e91ae2cc7e5d59da27d539455d1a0e1fb1b19
Tkaldahl <tkaldahl90@gmail.com>
2020-02-04 09:02:31 -0500

    place recipe list and shopping list components on html page

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe-book',
+  selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
